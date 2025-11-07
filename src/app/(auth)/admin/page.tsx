700006a3d669d6efb284762561d4f91a5f298e75
chyngyz-sultanaliev <sultanalievcyngyz98@gmail.com>
2025-11-07 19:03:44 +0600

    admin

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -1,19 +1,5 @@
-import Admin, { Cat } from "@/src/components/page/admin/Admin";
-import token from "@/config/api";
+import Admin from "@/src/components/page/admin/Admin";
 
 export default async function Page() {
-  const res = await fetch("http://localhost:5000/api/v1/cats", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  const data = await res.json();
-
-  // если API возвращает объект с массивом cats
-  const cats: Cat[] = Array.isArray(data) ? data : data.cats || [];
-
-  return <Admin initialCats={cats} />;
+  return <Admin />;
 }
